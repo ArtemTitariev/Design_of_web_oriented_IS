db23cf7990af25db9911fc1243917d72bd0f7109
Artem Titariev <artem_titariev@ukr.net>
2023-09-12 12:37:32 +0300

    task 4: finish

--- lab1/task4/class.php
+++ lab1/task4/class.php
@@ -1,24 +1,52 @@
 <?php
 class Coor
 {
-	private $text;
+	private $name;
+	private $login;
+	private $password;
 
-	public function __construct($text) // function for setting name  
+	public function __construct(string $name, string $login, string $password) // function for setting attributes  
 	{
-		$this->text = $text; //set some “text” to this “text”;
+		$this->name = $name; //set name;
+		$this->login = $login; // set login
+		$this->password = $password; //set pass
 	}
 
 	public function get_name()  //function for getting name
 	{
-		echo "<p>Name: " . $this->text . "<br>"; // printing name  
+		echo "<p>Name: " . $this->name . "</p>";
 	}
 
-	
+	public function get_login()  //function for getting name
+	{
+		echo "<p>Login: " . $this->login . "</p>";
+	}
+
+	public function get_password()  //function for getting name
+	{
+		echo "<p>Password: " . $this->password . "</p>";
+	}
+
+	public function __destruct() //destructor
+	{
+		echo "<p>Destroyed " . $this->name . "</p>";
+	}
 }
 
-$object = new Coor("Nick"); //creating “Coor” object
+$nick = new Coor("Nick", "Nick", "123456"); //creating “Coor” object
+$nick->get_name(); //print attributes
+$nick->get_login();
+$nick->get_password();
+echo '<br>';
 
-$object->get_name(); //function call
+$jack = new Coor("Jack", "J32n", "111111"); //creating “Coor” object
+$jack->get_name(); //print attributes
+$jack->get_login();
+$jack->get_password();
+echo '<br>';
 
-unset($object); //object destruction
-echo "Object is deleted!";
+$anna = new Coor("Anna", "Usr2ewd6", "654321"); //creating “Coor” object
+$anna->get_name(); //print attributes
+$anna->get_login();
+$anna->get_password();
+echo '<br>';
