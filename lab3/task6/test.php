edaa0554b3ca73216438ce492e70f37f71e1d8ee
Artem Titariev <artem_titariev@ukr.net>
2023-10-02 22:11:58 +0300

    modified code for test

--- lab3/task6/test.php
+++ lab3/task6/test.php
@@ -4,19 +4,22 @@ require_once 'Square.php';
 require_once 'Triangle.php';
 
 $circle = new Circle(1, 1, 10, 'black');
+$circle->draw();
 $circle->move(10, 10);
 $circle->draw();
 $circle->erase();
 echo '<br><br>';
 
 $square = new Square(0, 0, 10, 10, 'yellow');
-$square->move(10, -4);
+$square->draw();
+$square->move(-2, -4);
 $square->draw();
 $square->erase();
 echo '<br><br>';
 
-$triangle = new Triangle(0, 0, 10, 10, 2, 2, 'white');
-$triangle->move(10, -4);
+$triangle = new Triangle(1.4, 0.5, 5.2, 5.5, 20.8, 19.1, 'white');
+$triangle->draw();
+$triangle->move();
 $triangle->draw();
 $triangle->erase();
 
