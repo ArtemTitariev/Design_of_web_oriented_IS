dd9f9fe7c197640667956a18c87d7d0fba5e49bf
Artem Titariev <artem_titariev@ukr.net>
2023-11-24 21:59:44 +0200

    chenged model templates

--- lab8/model/Book.php
+++ lab8/model/Book.php
@@ -3,11 +3,23 @@
 	public $title;
 	public $author;
 	public $description;
+	public $publicationDate;
+	public $numberOfPages;
+	public $coverPhoto;
 
-	public function __construct($title, $author, $description)
-	{
+	public function __construct(
+		String $title,
+		string $author,
+		string $description,
+		DateTime $publicationDate,
+		int $numberOfPages,
+		string $coverPhoto
+	) {
 		$this->title = $title;
 		$this->author = $author;
 		$this->description = $description;
+		$this->publicationDate = $publicationDate;
+		$this->numberOfPages = $numberOfPages;
+		$this->coverPhoto = $coverPhoto;
 	}
 }
--- lab8/model/Model.php
+++ lab8/model/Model.php
@@ -5,19 +5,52 @@ class Model
 {
 	public function getBookList()
 	{
-		// here goes some hardcoded values to simulate the database           
-		return array(
-			"Jungle Book" => new Book("Jungle Book", "R. Kipling", "A classic book."),
-			"Moonwalker" => new Book("Moonwalker", "J. Walker", ""),
-			"PHP for Dummies" => new Book(
-				"PHP for Dummies",
-				"Some Smart Guy",
-				""
-			)
-		);
+
+		include("./model/db.php");
+		$sql = 'SELECT * FROM books 
+		JOIN authors ON books.author_id = authors.id
+		ORDER BY books.id';
+		$sth = $pdo->prepare($sql, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
+		$sth->execute();
+		$data = $sth->fetchAll();
+
+		$books = array();
+		for ($i = 0; $i < count($data); ++$i) {
+			$books[$data[$i]["title"]] = new Book(
+				$data[$i]["title"],
+				$data[$i]["name"] . ' ' . $data[$i]["surname"],
+				$data[$i]["description"],
+				new DateTime($data[$i]["publication_date"]),
+				$data[$i]["number_of_pages"],
+				$data[$i]["cover_photo"]
+			);
+		}
+		return $books;
 	}
+
 	public function getBook($title)
 	{
+		include("./model/db.php");
+		$sql = 'SELECT * FROM books 
+		JOIN authors ON books.author_id = authors.id
+		WHERE books.title = ?';
+		$sth = $pdo->prepare($sql, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
+		$sth->bindParam(1, $title, PDO::PARAM_STR);
+		$sth->execute();
+		//$sth->execute([$title]);
+
+		$data = $sth->fetchAll()[0];
+
+		return new Book(
+			$data["title"],
+			$data["name"] . ' ' . $data["surname"],
+			$data["description"],
+			new DateTime($data["publication_date"]),
+			$data["number_of_pages"],
+			$data["cover_photo"]
+		);
+
+
 		// we use the previous function to get all the books and then we return the requested one.   
 		// in a real life scenario this will be done through a db select command   
 		$allBooks = $this->getBookList();
