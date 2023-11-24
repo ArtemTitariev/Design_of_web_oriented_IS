da181c46bf586b18f54e78a1121a9f672273f5db
Artem Titariev <artem_titariev@ukr.net>
2023-11-24 22:25:04 +0200

    lab 8: changed model.php

--- lab8/model/Model.php
+++ lab8/model/Model.php
@@ -49,11 +49,5 @@ class Model
 			$data["number_of_pages"],
 			$data["cover_photo"]
 		);
-
-
-		// we use the previous function to get all the books and then we return the requested one.   
-		// in a real life scenario this will be done through a db select command   
-		$allBooks = $this->getBookList();
-		return $allBooks[$title];
 	}
 }
